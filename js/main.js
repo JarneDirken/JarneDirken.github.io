19a309914f6c4ff9618a8d9a166465601b98f927
JarneDirken <r0882161@student.thomasmore.be>
2021-12-30 16:28:19 +0100

    css 6 done

--- js/main.js
+++
@@ -1,8 +0,0 @@
-function myFunction() {
-  var x = document.getElementById("nav");
-  if (x.className === "site-nav") {
-    x.className += " responsive";
-  } else {
-    x.className = "site-nav";
-  }
-}
